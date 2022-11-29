e93a2a24a72462403fd8a2ec9ffb682161a0b866
Oleksandr <porpup@hotmail.com>
2022-11-28 19:38:39 -0500

    Added Content Search Page

--- js/actions.js
+++ js/actions.js
@@ -54,6 +54,10 @@ function logOut() {
 }
 
 
+function searchContent() {
+    window.location.href = "page8.html";
+}
+
 
 class Form {
     constructor(fName, lName, email, phone, message) {
@@ -129,3 +133,52 @@ function GetCookie(cname) {
     }
     return "";
 }
+
+
+
+
+
+
+
+
+
+// function checkPlanet() {
+//     //arrays containing data for 3 planets
+//     var arrNames = ["sun", "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+//     var arrDias = ["1,390,000 km", "4,880 km", "12,103.6 km", "12,756.3 km", "6,794 km", "142,984 km",
+//         "120,536 km", "51,118 km", "49,532 km"];
+//     var arrDistances = ["Temperature: 5800 K (surface) 15,600,000 K (core)", "Orbit: 57,910,000 km (0.38 AU) from Sun",
+//         "Orbit: 108,200,000 km (0.72 AU) from Sun", "Orbit: 149,600,000 km (1.00 AU) from Sun",
+//         "Orbit: 227,940,000 km (1.52 AU) from Sun", "Orbit: 778,330,000 km (5.20 AU) from Sun",
+//         "Orbit: 1,429,400,000 km (9.54 AU) from Sun", "Orbit: 2,870,990,000 km (19.218 AU) from Sun",
+//         "Orbit: 4,504,000,000 km (30.06 AU) from Sun"];
+//     var arrImgs = ["./Img/Sun.jpg", "./Img/Mercury.jpg", "./Img/Venus.jpg", "./Img/Earth.jpg", "./Img/Mars.jpg",
+//         "./Img/Jupiter.jpg", "./Img/Saturn.jpg", "./Img/Uranus.jpg", "./Img/Neptune.jpg"];
+
+//     var result, nPlanet, imgPlanet;
+//     nPlanet = document.getElementById("PName").value.toLowerCase();
+
+//     var index = -1;
+//     var planetName;
+
+//     for (var i = 0; i < arrNames.length; i++) {  //using loop for the search
+//         if (arrNames[i] == nPlanet) {
+//             index = i;
+//             result = "Planet: " + arrNames[index].toUpperCase() +
+//                 ", having Diameter of: " + arrDias[index] +
+//                 " and " + arrDistances[index];
+//             imgPlanet = arrImgs[index];
+//             planetName = arrNames[index].toUpperCase();
+//         }
+//     }
+
+//     if (index == -1) {  //no planet name was found
+//         result = "Found no planet with this name!";
+//         imgPlanet = "./Img/none.jpg";
+//     }
+
+//     document.getElementById("Res").innerHTML = result;
+//     document.getElementById("Rimg").src = imgPlanet;
+//     var cookiePlanetContent = planetName + "|" + Date.now();
+//     SetCookie("cookiePlanet", cookiePlanetContent, 10, "/");
+// }
